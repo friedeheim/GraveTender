0518320eac3621e86966bdeca3b14947d40c61d5
ElisabethDiAngelo <156357035+ElisabethDiAngelo@users.noreply.github.com>
2024-03-12 17:51:34 +0100

    Laufanimation final

--- Grave&Tender/Assets/Scripts/CharacterMovementAnimation.cs
+++ Grave&Tender/Assets/Scripts/CharacterMovementAnimation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterMovementAnimation : MonoBehaviour
 {
     private Animator animator;
-    private float currentRotation = 0f; // Variable zur Speicherung des aktuellen Drehwinkels
+    public float moveSpeed = 3f; // Geschwindigkeit des Charakters
 
     void Start()
     {
@@ -15,48 +15,29 @@ public class CharacterMovementAnimation : MonoBehaviour
 
     void Update()
     {
-        // Überprüfen, ob die "D" oder "Rechts-Pfeiltaste" gedrückt wird
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        // Bewegungsrichtung des Charakters
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        // Rotation des Charakters
+        if (moveDirection != Vector3.zero)
         {
-            // Rotation auf 90 Grad setzen, wenn die aktuelle Rotation nicht bereits 90 Grad ist
-            if (currentRotation != 90f)
-            {
-                transform.rotation = Quaternion.Euler(0, 90, 0);
-                currentRotation = 90f;
-            }
-            // Walking-Animation abspielen
-            animator.Play("walking");
+            transform.rotation = Quaternion.LookRotation(moveDirection);
         }
 
-        // Überprüfen, ob die "A" oder "Links-Pfeiltaste" gedrückt wird
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            // Rotation auf -90 Grad setzen, wenn die aktuelle Rotation nicht bereits -90 Grad ist
-            if (currentRotation != -90f)
-            {
-                transform.rotation = Quaternion.Euler(0, -90, 0);
-                currentRotation = -90f;
-            }
-            // Walking-Animation abspielen
-            animator.Play("walking");
-        }
+        // Bewegung des Charakters relativ zur Kamera
+        Vector3 move = moveDirection * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
 
-        // Überprüfen, ob die "W", "Oben-Pfeiltaste" oder "Numpad 8" gedrückt wird
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Keypad8))
+        // Animation abhängig von der Bewegung abspielen
+        if (moveDirection.magnitude > 0)
         {
-            // Bewegung in Vorwärtsrichtung relativ zur lokalen Orientierung des Objekts
-            transform.Translate(transform.forward * Time.deltaTime);
-            // Walking-Animation abspielen
-            animator.Play("walking");
+            animator.SetBool("Walking", true);
         }
-
-        // Überprüfen, ob die "S", "Unten-Pfeiltaste" oder "Numpad 2" gedrückt wird
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.Keypad2))
+        else
         {
-            // Bewegung in Rückwärtsrichtung relativ zur lokalen Orientierung des Objekts
-            transform.Translate(-transform.forward * Time.deltaTime);
-            // Walking-Animation abspielen
-            animator.Play("walking");
+            animator.SetBool("Walking", false);
         }
     }
 }
